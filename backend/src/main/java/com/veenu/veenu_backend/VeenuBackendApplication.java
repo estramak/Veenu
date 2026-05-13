472eef50013e5a8a62044c70122526e8fdd79b69
estramak <114385631+estramak@users.noreply.github.com>
2026-05-12 17:34:27 -0700

    Add Spring Boot backend scaffold

---
+++ backend/src/main/java/com/veenu/veenu_backend/VeenuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veenu.veenu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeenuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeenuBackendApplication.class, args);
+	}
+
+}
