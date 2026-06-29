5c148b59462fe6aca2e66493a8ae37aab16fbb99
estramak <114385631+estramak@users.noreply.github.com>
2026-06-28 20:41:50 -0700

    Initial commit without model folder

---
+++ Veenu/src/main/java/estramak/Veenu/VeenuApplication.java
@@ -0,0 +1,13 @@
+package estramak.Veenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeenuApplication.class, args);
+	}
+
+}
