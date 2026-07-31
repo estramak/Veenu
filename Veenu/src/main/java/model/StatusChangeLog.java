ab3b806bef66d6b9f208be42dcd7ead6426c23ef
estramak <114385631+estramak@users.noreply.github.com>
2026-07-30 19:11:33 -0700

    feat: add StatusChangeLog Entity

---
+++ Veenu/src/main/java/model/StatusChangeLog.java
@@ -0,0 +1,50 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import model.enums.AdminEntityType;
+import model.enums.EntityStatus;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "status_change_log")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class StatusChangeLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AdminEntityType entityType;
+
+    @Column(nullable = false)
+    private Long entityId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EntityStatus newStatus;
+
+    @Column(length = 500)
+    private String reason;
+
+    @Column(length = 1000)
+    private String adminNotes;
+
+    @Column(nullable = false)
+    private LocalDateTime changedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        changedAt = LocalDateTime.now();
+    }
+}
---
+++ Veenu/src/main/java/model/enums/AdminEntityType.java
@@ -0,0 +1,8 @@
+package model.enums;
+
+public enum AdminEntityType {
+    BUSINESS,
+    LISTING,
+    USER,
+    NOTE
+}
